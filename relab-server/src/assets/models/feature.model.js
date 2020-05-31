cc39dc7f87b599ddd9b6f9359561096cc5391879
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-05-31 17:22:46 +0000

    fase prima dei 4 esercizi

--- relab-server/src/assets/models/feature.model.js
+++ relab-server/src/assets/models/feature.model.js
@@ -1,16 +1,15 @@
-//Classe utile a rappresentare le feature degli oggetti geojson
 module.exports = class Feature {
-    constructor(id, geometry) {
+    constructor(id, geometry, media, somma) {
         this.type = "Feature";
-        //Per ora le proprietà contengono un solo valore (id)
-        this.properties = new Properties(id);
-        //Contiene la geometria del poligono.
+        this.properties = new Properties(id, media, somma);
         this.geometry = geometry;
     }
 }
-//Iniziamo a preparare la classe Properties che complicheremo in seguito
+
 class Properties {
-    constructor(id) {
+    constructor(id, media, somma) {
         this.id = id;
+        this.media = media;
+        this.somma = somma;
     }
 }
